20b9602229c8c03ed88ddb6996c6e1920ca379f1
Thiago Rodrigues <thiag0ferreira@hotmail.com>
2024-06-24 14:23:05 -0300

    Padronizado Css

--- adotaPet/adotapet-backend/server.js
+++ adotaPet/adotapet-backend/server.js
@@ -10,7 +10,8 @@ app.use(bodyParser.json());
 // Conectar ao MongoDB
 mongoose.connect('mongodb://localhost:27017/adotapet', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  serverSelectionTimeoutMS: 5000
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
